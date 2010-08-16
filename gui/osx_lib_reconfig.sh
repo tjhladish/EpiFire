257f2983d22525300101b2678f327aedce69206c
tjhladish <tjhladish@tjhladishs-Mac-mini.local>
2010-08-16 00:18:18 -0500

    Adding library config file for OSX
    Run this shell script In order to compile the osx version using distributed (rather than global)
    copies of the Qt libraries.  This makes the install file quite a bit bigger than it would be
    otherwise, but this way users don't have to install the much larger Qt libraries.  Also,
    dependency problems for end-users are less likely if they're already getting the right
    versions of the Qt libraries.

---
+++ gui/osx_lib_reconfig.sh
@@ -0,0 +1,19 @@
+#/usr/bin/qmake /Users/tjhladish/EpiFire/gui/epifire.pro -spec macx-g++ -macx -r CONFIG+=release -o Makefile
+#make -j2
+#otool -L QtGui.framework/QtGui
+#otool -L /Library/Frameworks/QtGui.framework/QtGui
+#mkdir epifire.app/Contents/Frameworks
+#cp -R /Library/Frameworks/QtCore.framework ./epifire.app/Contents/Frameworks
+#cp -R /Library/Frameworks/QtGui.framework ./epifire.app/Contents/Frameworks
+#/usr/bin/qmake /Users/tjhladish/EpiFire/gui/epifire.pro -spec macx-g++ -macx -r CONFIG+=release -o Makefile
+#make -j2
+install_name_tool -id @executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore epifire.app/Contents/Frameworks/QtCore
+install_name_tool -id @executable_path/../Frameworks/QtGui.framework/Versions/4/QtGui epifire.app/Contents/Frameworks/QtGui
+install_name_tool -change /Library/Frameworks/QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore epifire.app/Contents/MacOS/epifire 
+install_name_tool -change /Library/Frameworks/QtGui.framework/Versions/4/QtGui @executable_path/../Frameworks/QtGui epifire.app/Contents/MacOS/epifire 
+install_name_tool -change /Library/Frameworks/QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore epifire.app/Contents/Frameworks/QtGui
+
+
+install_name_tool -change QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore epifire.app/Contents/MacOS/epifire 
+install_name_tool -change QtGui.framework/Versions/4/QtGui @executable_path/../Frameworks/QtGui epifire.app/Contents/MacOS/epifire 
+install_name_tool -change QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore epifire.app/Contents/Frameworks/QtGui
