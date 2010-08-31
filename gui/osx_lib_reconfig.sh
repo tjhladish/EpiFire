63a320179c66c560b1fe1ae5a402ef3be4d0345b
tjhladish <tjhladish@tjhladishs-Mac-mini.local>
2010-08-31 13:00:24 -0500

    Cleaned up OSX compilation script

--- gui/osx_lib_reconfig.sh
+++ gui/osx_lib_reconfig.sh
@@ -2,13 +2,13 @@
 #make -j2
 #otool -L QtGui.framework/QtGui
 #otool -L /Library/Frameworks/QtGui.framework/QtGui
-#mkdir epifire.app/Contents/Frameworks
-#cp -R /Library/Frameworks/QtCore.framework ./epifire.app/Contents/Frameworks
-#cp -R /Library/Frameworks/QtGui.framework ./epifire.app/Contents/Frameworks
-#/usr/bin/qmake /Users/tjhladish/EpiFire/gui/epifire.pro -spec macx-g++ -macx -r CONFIG+=release -o Makefile
-#make -j2
-install_name_tool -id @executable_path/../Frameworks/QtCore.framework/Versions/4/QtCore epifire.app/Contents/Frameworks/QtCore
-install_name_tool -id @executable_path/../Frameworks/QtGui.framework/Versions/4/QtGui epifire.app/Contents/Frameworks/QtGui
+mkdir -p epifire.app/Contents/Frameworks
+cp /Library/Frameworks/QtCore.framework/Versions/4/QtCore ./epifire.app/Contents/Frameworks/
+cp /Library/Frameworks/QtGui.framework/Versions/4/QtGui ./epifire.app/Contents/Frameworks/
+/usr/bin/qmake /Users/tjhladish/EpiFire/gui/epifire.pro -spec macx-g++ -macx -r CONFIG+=release -o Makefile
+make -j2
+install_name_tool -id @executable_path/../Frameworks/QtCore epifire.app/Contents/Frameworks/QtCore
+install_name_tool -id @executable_path/../Frameworks/QtGui epifire.app/Contents/Frameworks/QtGui
 install_name_tool -change /Library/Frameworks/QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore epifire.app/Contents/MacOS/epifire 
 install_name_tool -change /Library/Frameworks/QtGui.framework/Versions/4/QtGui @executable_path/../Frameworks/QtGui epifire.app/Contents/MacOS/epifire 
 install_name_tool -change /Library/Frameworks/QtCore.framework/Versions/4/QtCore @executable_path/../Frameworks/QtCore epifire.app/Contents/Frameworks/QtGui
