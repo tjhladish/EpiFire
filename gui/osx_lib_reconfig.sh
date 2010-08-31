c7cb7068d5f710c8742b014818f4b8136f081396
tjhladish <tjhladish@tjhladishs-Mac-mini.local>
2010-08-31 13:06:40 -0500

    New OSX version 2.13

--- gui/osx_lib_reconfig.sh
+++ gui/osx_lib_reconfig.sh
@@ -2,9 +2,9 @@
 #make -j2
 #otool -L QtGui.framework/QtGui
 #otool -L /Library/Frameworks/QtGui.framework/QtGui
-mkdir -p epifire.app/Contents/Frameworks
-cp /Library/Frameworks/QtCore.framework/Versions/4/QtCore ./epifire.app/Contents/Frameworks/
-cp /Library/Frameworks/QtGui.framework/Versions/4/QtGui ./epifire.app/Contents/Frameworks/
+#mkdir -p epifire.app/Contents/Frameworks
+#cp /Library/Frameworks/QtCore.framework/Versions/4/QtCore ./epifire.app/Contents/Frameworks/
+#cp /Library/Frameworks/QtGui.framework/Versions/4/QtGui ./epifire.app/Contents/Frameworks/
 /usr/bin/qmake /Users/tjhladish/EpiFire/gui/epifire.pro -spec macx-g++ -macx -r CONFIG+=release -o Makefile
 make -j2
 install_name_tool -id @executable_path/../Frameworks/QtCore epifire.app/Contents/Frameworks/QtCore
