012847a85486008a24bda477752e9f7d7dc623a5
Tom Hladish <tjhladish@mail.utexas.edu>
2012-05-10 22:00:41 -0500

    I think the deterministic network simulator is now implemented correctly; needs to be validated

--- src/Deterministic_Network_SIR_Sim.h
+++ src/Deterministic_Network_SIR_Sim.h
@@ -4,6 +4,7 @@
 #include <gsl/gsl_odeiv.h>
 #include <gsl/gsl_errno.h>
 #include <iostream>
+#include <math.h>
 #include "DiffEq_Sim.h"
 
 using namespace std;
@@ -13,23 +14,55 @@ class Deterministic_Network_SIR_Sim : public DiffEq_Sim {
     private:
         const double r;
         const double mu;
+        const vector<double> deg_dist;
 
     public:
-        Deterministic_Network_SIR_Sim() : r(0.0), mu(0.0) { nbins=3;}
-        Deterministic_Network_SIR_Sim(double r_param, double mu_param): r(r_param), mu(mu_param) { nbins=3; }
+        Deterministic_Network_SIR_Sim() : r(0.0), mu(0.0) { nbins=4;}
+        Deterministic_Network_SIR_Sim(double r_param, double mu_param, vector<double> deg_dist_param): r(r_param), mu(mu_param), deg_dist(deg_dist_param) { nbins=4; }
         ~Deterministic_Network_SIR_Sim() {};
 
-        void initialize( double theta, double pS, double pI) {
+        void initialize( double theta, double pS, double pI, double I) {
             y = new double[nbins];
             y[0] = theta; 
             y[1] = pS; 
             y[2] = pI;
+            y[3] = I;
         }
 
-        double g(double theta)  { return .25*pow(theta,2) + .5*pow(theta,3) + .25*pow(theta,4); }
-        double dg(double theta) { return .5*theta + 1.5*pow(theta,2) + 1*pow(theta,3); }
-        double ddg(double theta){ return .5 + 3*theta  + 3*pow(theta,2); }
+        double current_susceptible() { return g( y[0] ); }
+        double current_infectious() { return y[3]; }
+        double current_recovered() { return 1.0 - current_susceptible() - current_infectious(); }
 
+        double g( double theta) {
+            double val = 0;
+            for (unsigned int i = 0; i<deg_dist.size(); i++) {
+                val += deg_dist[i] * pow(theta,i);
+            }
+            return val;
+        }
+
+        double dg( double theta) {
+            double val = 0;
+            for (unsigned int i = 1; i<deg_dist.size(); i++) {
+                val += (i) * deg_dist[i] * pow(theta,i-1);
+            }
+            return val;
+        }
+
+        double ddg( double theta) {
+            double val = 0;
+            for (unsigned int i = 2; i<deg_dist.size(); i++) {
+                val += (i) * (i-1) *deg_dist[i] * pow(theta,i-2);
+            }
+            return val;
+        }
+/*
+        g(x) = p_0 + p_1 * x + p_2 *x^2
+
+        double g(double theta)  { return 0*pow(theta,0) + 0*pow(theta,1) + .25*pow(theta,2) + .5*pow(theta,3)  + .25*pow(theta,4); }
+        double dg(double theta) { return                1*0*pow(theta,0) + .5*pow(theta,1)  + 1.5*pow(theta,2) + 1*pow(theta,3); }
+        double ddg(double theta){ return                                   .5*pow(theta,0)  + 3*pow(thetha,1)  + 3*pow(theta,2); }
+*/
         void derivative(double const y[], double dydt[]) {
             //dydt[0] = -beta*y[0]*y[1];
             //dydt[1] = +beta*y[0]*y[1] - gamma*y[1];
@@ -38,11 +71,13 @@ class Deterministic_Network_SIR_Sim : public DiffEq_Sim {
             const double theta = y[0];
             const double pS = y[1];
             const double pI = y[2];
+            const double I = y[3];
 
             dydt[0] = -r * pI * theta;                                                  // dtheta.dt
             dydt[1] = r *  pS * pI * (1 - theta * ddg(theta)/dg(theta));                // dpS.dt
             dydt[2] = r *  pI * pS * theta * ddg(theta)/dg(theta) - pI*(1-pI)*r- pI*mu; // dpI.dt
 
+            dydt[3] = r * pI * theta * dg(theta) - mu*I;
         }
 
 };
