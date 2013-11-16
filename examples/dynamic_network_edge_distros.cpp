aac3ce33e9203ccf8d70fd3a1b9c56a558e654ff
Thomas Hladish <tjhladish@utexas.edu>
2013-11-16 16:22:14 -0500

    Deleting commented code

--- examples/dynamic_network_edge_distros.cpp
+++ examples/dynamic_network_edge_distros.cpp
@@ -18,11 +18,6 @@ int main(int argc, char* argv[]) {
    		cout << endl;
 
     	net.clear_edges();
-
-//        Gillespie_Network_SEIRS_Sim sim(&net, mu, beta, gamma, immunity_duration);
-//        sim.rand_infect(1);
-//        sim.run_simulation(1000);
-        //cout << sim.current_epidemic_size() << endl;
     }
 
     return 0;
--- examples/mod_ex7_gillespie_network_SEIRS.cpp
+++ examples/mod_ex7_gillespie_network_SEIRS.cpp
@@ -12,11 +12,6 @@ int main(int argc, char* argv[]) {
 
     ifstream source(filename.c_str());
 
-//    double mu    = 1.0/2.0; // E -> I transition rate
-//    double beta  = 1.0/3.0; // contact rate
-//    double gamma = 1.0/6.0; // I -> R transition rate
-//    double immunity_duration = 365;
-
     while(net.add_edgelist(source)) {
 
     	vector<vector<Node*> > comps = net.get_components();
@@ -28,11 +23,6 @@ int main(int argc, char* argv[]) {
    		cout << endl;
 
     	net.clear_edges();
-
-//        Gillespie_Network_SEIRS_Sim sim(&net, mu, beta, gamma, immunity_duration);
-//        sim.rand_infect(1);
-//        sim.run_simulation(1000);
-        //cout << sim.current_epidemic_size() << endl;
     }
 
     return 0;
